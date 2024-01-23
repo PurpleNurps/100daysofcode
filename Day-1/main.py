a08de4696f43b6e656c244a2fb7c36184606113a
PurpleNurps <seanlunt1991@gmail.com>
2024-01-23 19:25:52 +0000

    day 1 nearly finished. Did some printing and learned about input

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,5 +1,6 @@
 # Provide any name in the input pane below.
 # That value can be accessed using the input() function.
 # Don't put anything inside the input() function!
+print("Hello" + " Angela")
 print("Hello, " + input() + "!")
 print(len(input()))
